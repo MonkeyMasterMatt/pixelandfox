0727987ad9e5447d859474f1934abe2b8ef7124f
Matt Pitt <matt.ianpitt@gmail.com>
2018-09-25 15:31:37 +0200

    try out liquid banner effect

---
+++ app/javascript/components/rotate_left_ear.js
@@ -0,0 +1,12 @@
+// let rotated = false;
+
+// setInterval(function(){
+//   let earLeft = document.querySelector('.ear-left');
+//   deg = rotated ? 0 : 90;
+//     earLeft.style.webkitTransform = 'rotate('+deg+'deg)';
+//     earLeft.style.mozTransform    = 'rotate('+deg+'deg)';
+//     earLeft.style.msTransform     = 'rotate('+deg+'deg)';
+//     earLeft.style.oTransform      = 'rotate('+deg+'deg)';
+//     earLeft.style.transform       = 'rotate('+deg+'deg)';
+//     rotated = !rotated;
+//  }, 7500);
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,3 @@
 import "bootstrap";
+import "components/rotate_shapes";
+import "components/rotate_left_ear";
